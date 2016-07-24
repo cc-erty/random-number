4b099f89c58562ed2b1adb33cdea2024e40c5ce8
Erty Seidohl <erty@codecraftschool.com>
2016-07-24 13:09:13 -0600

    Add cors

--- random.js
+++ random.js
@@ -3,6 +3,7 @@ var http = require('http');
 var port = 8001;
 
 function handleRequest(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.write("" + (Math.floor(Math.random() * 10) + 1));
 	res.end();
 }
