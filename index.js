b79b56b079f6cd94b761960f0f8718ff1d0e1009
Erty Seidohl <erty@codecraftschool.com>
2016-07-24 01:08:30 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var port = 8001;
+
+function handleRequest(req, res) {
+	res.write("" + (Math.floor(Math.random() * 10) + 1));
+	res.end();
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(port);
+
+console.log("Server started on port " + port);
+
+
